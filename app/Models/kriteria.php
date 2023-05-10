0abe0dc1ff9733d451254bae03946b0443b0e196
novirachmahwati <novirachmahwati@gmail.com>
2023-05-10 16:56:07 +0700

    progress 10 mei 2023

--- app/Models/kriteria.php
+++ app/Models/kriteria.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class kriteria extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'pendidikan',
+        'skill',
+        'pengalaman_kerja',
+        'penghargaan',
+        'organisasi',
+        'bahasa',
+        'deskripsi',
+        'foto',
+        'lowongan_kerja_id'
+    ];
+
+    protected $appends = [
+        'name',
+        'email'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
